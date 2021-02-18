36db02b05340011a92ecbc11030f2ca3dbb5c543
Simon Lock <simon.lock@bristol.ac.uk>
2021-02-18 18:50:33 +0000

    Another change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Bye World');
+  res.end('Goodbye Everyone');
 });
 
 server.listen(port, hostname, () => {
