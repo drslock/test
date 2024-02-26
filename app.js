66ee46018c253fdbac892572d3880bcaa5b7dee8
Simon Lock <simon.lock@bristol.ac.uk>
2024-02-26 11:32:23 +0000

    Tweaks

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Goodbye Everyone');
+  res.end('Goodbye Everyone Else');
 });
 
 server.listen(port, hostname, () => {
